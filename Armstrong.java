156d12b76c3c209c6e84fd437aadcad9e7ebd9e7
pavani37 <92800034+pavani37@users.noreply.github.com>
2022-02-26 10:18:17 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Armstrong{
+    public  static void main(String args[]){
+        int num,temp,c=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number:");
+        num=sc.nextInt();
+        temp=num;
+        while(num!=0){
+            int d=num%10;
+            c=c+(d*d*d);
+            num=num/10;
+        }
+        if(temp==c){
+            System.out.println("Given number is armstrong");
+        }
+        else{
+            System.out.println("Given number is not armstrong");
+        }
+    }
+}
